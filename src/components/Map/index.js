bea4f0d7085752b1f1ea34cc27bac0d1647b55b1
Jeff St. Jean <jeffrey@stjean.ca>
2019-11-23 13:24:13 -0500

    init

---
+++ src/components/Map/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { API_KEY } from '../../../src/secrets.js'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { geolocated } from "react-geolocated";
+
+class MapContainer extends React.Component {
+  render() {
+    console.log(`Available: ${this.props.isGeolocationAvailable}`)
+    console.log(`Enabled: ${this.props.isGeolocationEnabled}`)
+    return (
+      <Map
+      google={this.props.google}
+      zoom={14}
+      initialCenter={{
+            lat: 40.854885,
+            lng: -88.081807
+      }}
+      >
+
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>Hi</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+const LoadingContainer = (props) => (
+  <div>Fancy loading container!</div>
+)
+
+export default GoogleApiWrapper({
+  apiKey: API_KEY,
+  LoadingContainer: LoadingContainer
+})(MapContainer)
