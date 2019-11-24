d1a6308469c18532e252e7a751837c90c074a2a8
Sanjayan Kulendran <skulend2@uwo.ca>
2019-11-23 23:14:11 -0500

    Revises loading message

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -3,9 +3,18 @@ import React from 'react';
 export default class MapContainer extends React.Component {
   render() {
     return (
-      <div className="LoadingComponent">
-        <h1>Loading</h1>
-      </div>
-    );
+      <div className="LoadingComponent"
+      style={{
+          color: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: '20vh',
+          right: '7vw',
+          paddingLeft: '5vw',
+          fontSize: "12vh",
+          fontWeight: "lighter",
+      }}
+    >loading...</div> )
   }
 }
