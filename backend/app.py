d27b332ce46d867b04f92c35f86566f4ac204ae4
Carl Baron <carbaro196@gmail.com>
2019-11-23 20:25:43 -0500

    Finished safe path route

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,10 @@ from datetime import datetime
 import secrets
 import json
 import operator
+import opendata
+import urllib.request
+import requests
+
 # declare constants
 HOST = '0.0.0.0'
 PORT = 5000
@@ -22,15 +26,14 @@ DEBUG = True
 
 # Safest path
 # Usage: IP:HOST/api/path?from=LAT,LON&to=LAT,LON
-# Example: http://0.0.0.0:5000/api/path?from=43.004663,-81.276361&to=42.993940,-81.276805
+# Example: http://0.0.0.0:5000/api/path?from=43.004663,-81.276361&to=248 Trott Dr
 @app.route('/api/path', methods=['GET'])
 def safe_path():
-    # Response header
-    headers = {"Content-Type": "application/json"}
-
     # Grab data from requests
     start = request.args.get('from')
     end = request.args.get('to')
+    end = gmaps.geocode(end)
+    end = (end[0]['geometry']['location']['lat'], end[0]['geometry']['location']['lng'])
     
     if DEBUG:
         print('Start coordinates: {}'.format(start))
@@ -47,38 +50,46 @@ def safe_path():
     print('Possible routes: {}'.format(len(routes)))
     
     seen_lights = [None] * len(routes)
+    tracked_lights = [None] * len(routes)
+
     total_lights = [0] * len(routes)
     for index, route in enumerate(routes):
         # For every route, calculate the possible lights
         ne_bound = (route['bounds']['northeast']['lat'], route['bounds']['northeast']['lng']) 
         sw_bound = (route['bounds']['southwest']['lat'], route['bounds']['southwest']['lng']) 
-
-        """ lights = opendata.get_lights(ne_bound, sw_bound) """
+        available_lights = opendata.queryAreaLights(ne_bound, sw_bound, 0.00025)
 
         # Decode polyline for waypoints
         waypoints = polyline.decode(route['overview_polyline']['points'])
+        # Add start and stop points to waypoints
+        waypoints.insert(0, (float(start[0]),float(start[1]) ) )
+        waypoints.append((float(end[0]),float(end[1]) ) )
 
-        for point in waypoints:
-
-            """ in_range = opendata.get_in_range(point, lights, 0.050) """
+        print('{} waypoints for route {}'.format(len(waypoints), index+1))
+        for i in range(len(waypoints) - 1):
+            point = waypoints[i]
+            next_point = waypoints[i+1]
             
-            in_range = [{'id': '1', 'head': 1}, {'id': '2', 'head': 2}, {'id': '1', 'head': 1}]
-
+            in_range = opendata.getSeenLights(point, next_point, available_lights, 0.0005)
             for rlight in in_range:
                 if not seen_lights[index]:
                     seen_lights[index] = set()
+                
+                if not tracked_lights[index]:
+                    tracked_lights[index] = list()
 
                 # If light has already been tracked for this path, dont track it again
                 if rlight['id'] not in seen_lights[index]:
                     # If not tracked, track it and increment the total count of lights for this route
                     seen_lights[index].add(rlight['id'])
+                    tracked_lights[index].append(rlight)
                     total_lights[index] += (1 * rlight['head'])
     
     max_light_density = 0
     max_index = -1
     max_dist = 0
 
-    print('Lights per route: {}'.format(total_lights))
+    print('Lights for each route: {}'.format(total_lights))
     for i in range(len(total_lights)):
         dist = routes[i]['legs'][0]['distance']['text']
         dist = dist.split(' ')            
@@ -97,10 +108,21 @@ def safe_path():
                 max_index = i
                 max_dist = dist
 
-    print('Weighted lights per route: {}'.format(total_lights))
-
+    print('Weighted lights for each route: {}'.format(total_lights))
     print('Best route: {}'.format(max_index+1))
-    resp = make_response(jsonify(routes), 200)
+    
+    # Return as a response a list of routes with their corresponding tracked lights and their bounding box lights and their safety rating
+    safety_result = []
+    for i in range(len(routes)):
+        safety_result.append( {
+            'rating': total_lights[i],
+            'polyline': routes[i]['overview_polyline']['points'],
+            'area_lights' : list(seen_lights[i]),
+            'in_range_lights': tracked_lights[i]
+        } )
+
+    safety_result = json.dumps(safety_result)
+    resp = make_response(safety_result, 200)
     return(resp)
 
 if __name__ == '__main__':
---
+++ backend/opendata.py
@@ -0,0 +1,54 @@
+import string
+import urllib.request, json
+from geopy.distance import geodesic
+import numpy as np
+from numpy import linalg as LA
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+#Returns a list of street lights in the given area
+def queryAreaLights(NorthEast, SouthWest, radius):
+
+    url_send = "https://maps.london.ca/arcgisa/rest/services/OpenData/OpenData_Transportation/MapServer/19/query?where=1%3D1&outFields=PublicCartoSymbolType,OBJECTID&geometry=" + str(SouthWest[1] - radius) + "%2C" + str(SouthWest[0] - radius) + "%2C" + str(NorthEast[1] + radius) + "%2C" + str(NorthEast[0] + radius) + "&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=4326&f=json"
+
+    with urllib.request.urlopen(url_send) as url:
+        data = json.loads(url.read().decode())
+        
+    return data["features"]
+
+#Gets seen lights at given point as a list of dictionaries, within certain radius in KM
+def getSeenLights(point, next_point, lights, radius):
+
+    #Calculate bounding box for waypoints: Frist get edge vector at 0,0, but perpendicular (get perpendicular vector {X, Y} becomes {x, -y})
+    edge = np.array([[0,0],[next_point[0] - point[0], point[1] - next_point[1]]])
+
+    #Get unit vector and multiply by desired radius (arbitrary units) and opposite vector
+    edge_perp_radius = np.true_divide(edge, LA.norm(edge))*radius
+    neg_edge_perp_radius = -edge_perp_radius
+
+    #Get four points for polygon using vector for edge on passed points
+    point_1 = [point[0] + edge_perp_radius[1][1], point[1] + edge_perp_radius[1][0]]
+    point_2 = [point[0] + neg_edge_perp_radius[1][1], point[1] + neg_edge_perp_radius[1][0]]
+    point_3 = [next_point[0] + edge_perp_radius[1][1], next_point[1] + edge_perp_radius[1][0]]
+    point_4 = [next_point[0] + neg_edge_perp_radius[1][1], next_point[1] + neg_edge_perp_radius[1][0]]
+
+    lat_lon_area = np.array([point_1, point_2, point_4, point_3])
+
+    #Generate polygon representing area to check for lights
+    polygon = Polygon(lat_lon_area)
+
+
+    seen_lights = []
+
+    for light in lights:
+        light_loc = Point(light["geometry"]["y"], light["geometry"]["x"])
+        #light_loc = (light["geometry"]["y"], light["geometry"]["x"])
+        if polygon.contains(light_loc):
+            if not light["attributes"]["PublicCartoSymbolType"]:
+                val = 1
+            else:
+                val = int(light["attributes"]["PublicCartoSymbolType"].split('-')[1].split('H')[0])
+
+            seen_lights.append({ "id":light["attributes"]["OBJECTID"], "head":val, "latitude":light["geometry"]["y"], "longitude":light["geometry"]["x"]})
+
+    return seen_lights
