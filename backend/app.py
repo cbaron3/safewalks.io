7d8b0584fad029573ec1b1fac71045e1bc11ccdd
Carl Baron <carbaro196@gmail.com>
2019-11-23 22:20:16 -0500

    Ssssssshhhh its working

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,9 @@ import opendata
 import urllib.request
 import requests
 
+# Handling cross origin resource handling
+
+
 # declare constants
 HOST = '0.0.0.0'
 PORT = 5000
@@ -16,6 +19,9 @@ import polyline
 
 # initialize flask application
 app = Flask(__name__)
+
+from flask_cors import CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 # Google maps client
 
@@ -29,12 +35,13 @@ DEBUG = True
 # Example: http://0.0.0.0:5000/api/path?from=43.004663,-81.276361&to=248 Trott Dr
 @app.route('/api/path', methods=['GET'])
 def safe_path():
+    print(request.args)
     # Grab data from requests
     start = request.args.get('from')
     end = request.args.get('to')
     end = gmaps.geocode(end)
     end = (end[0]['geometry']['location']['lat'], end[0]['geometry']['location']['lng'])
-
+    
     if DEBUG:
         print('Start coordinates: {}'.format(start))
         print('End coordinates: {}'.format(end))
@@ -48,33 +55,34 @@ def safe_path():
                               departure_time=now)
 
     print('Possible routes: {}'.format(len(routes)))
-
+    
     seen_lights = [None] * len(routes)
     tracked_lights = [None] * len(routes)
 
     total_lights = [0] * len(routes)
     for index, route in enumerate(routes):
         # For every route, calculate the possible lights
-        ne_bound = (route['bounds']['northeast']['lat'], route['bounds']['northeast']['lng'])
-        sw_bound = (route['bounds']['southwest']['lat'], route['bounds']['southwest']['lng'])
+        ne_bound = (route['bounds']['northeast']['lat'], route['bounds']['northeast']['lng']) 
+        sw_bound = (route['bounds']['southwest']['lat'], route['bounds']['southwest']['lng']) 
         available_lights = opendata.queryAreaLights(ne_bound, sw_bound, 0.00025)
 
         # Decode polyline for waypoints
         waypoints = polyline.decode(route['overview_polyline']['points'])
         # Add start and stop points to waypoints
+        start = start.split(',')
         waypoints.insert(0, (float(start[0]),float(start[1]) ) )
         waypoints.append((float(end[0]),float(end[1]) ) )
-
+        print(waypoints)
         print('{} waypoints for route {}'.format(len(waypoints), index+1))
         for i in range(len(waypoints) - 1):
             point = waypoints[i]
             next_point = waypoints[i+1]
-
+            
             in_range = opendata.getSeenLights(point, next_point, available_lights, 0.0005)
             for rlight in in_range:
                 if not seen_lights[index]:
                     seen_lights[index] = set()
-
+                
                 if not tracked_lights[index]:
                     tracked_lights[index] = list()
 
@@ -84,7 +92,7 @@ def safe_path():
                     seen_lights[index].add(rlight['id'])
                     tracked_lights[index].append(rlight)
                     total_lights[index] += (1 * rlight['head'])
-
+    
     max_light_density = 0
     max_index = -1
     max_dist = 0
@@ -92,7 +100,7 @@ def safe_path():
     print('Lights for each route: {}'.format(total_lights))
     for i in range(len(total_lights)):
         dist = routes[i]['legs'][0]['distance']['text']
-        dist = dist.split(' ')
+        dist = dist.split(' ')            
         dist = float( dist[0] )
         total_lights[i] = total_lights[i]/dist
 
@@ -110,7 +118,7 @@ def safe_path():
 
     print('Weighted lights for each route: {}'.format(total_lights))
     print('Best route: {}'.format(max_index+1))
-
+    
     # Return as a response a list of routes with their corresponding tracked lights and their bounding box lights and their safety rating
     safety_result = []
     for i in range(len(routes)):
@@ -123,6 +131,8 @@ def safe_path():
 
     safety_result = json.dumps(safety_result)
     resp = make_response(safety_result, 200)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     return(resp)
 
 if __name__ == '__main__':
